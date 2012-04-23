21779eff06ddd58d155f27c5c801dd4bd503f727
Aritz Galdos <aritz@sareweb.net>
2012-04-23 20:07:08 +0200

    Created empty android project

---
+++ src/main/java/com/primum/mobile/activity/StartActivity.java
@@ -0,0 +1,28 @@
+package com.primum.mobile.activity;
+
+import com.primum.mobile.R;
+import com.primum.mobile.R.layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class StartActivity extends Activity {
+
+    private static String TAG = "Primum-Android";
+
+    /**
+     * Called when the activity is first created.
+     * @param savedInstanceState If the activity is being re-initialized after 
+     * previously being shut down then this Bundle contains the data it most 
+     * recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+		Log.i(TAG, "onCreate");
+        setContentView(R.layout.main);
+    }
+
+}
+
